62758e47d413e98cedf2d85098498490d087d666
erik <erik.blom@eleostech.com>
2021-02-26 10:41:14 -0500

    more api stuff

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from sponsor_app.models import Sponsor 
+from driver_app.models import Driver
 
 class SponsorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Sponsor 
         fields = ('sponsor_name','exchange_rate')
+class DriverSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Driver 
+        fields = ('name','address','sponsor','phone','credits')
